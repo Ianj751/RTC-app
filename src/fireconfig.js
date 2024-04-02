6ee12207f74fbf1c63d519d78baad65b3970cf1e
Ian Jackson <136002718+Ianj751@users.noreply.github.com>
2024-04-02 18:49:08 -0500

    Updated firebase.js

--- src/fireconfig.js
+++ src/fireconfig.js
@@ -9,14 +9,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB3WxeA5b-XKtSj-54P4vlEFr-EqHYdJmo",
-  authDomain: "my-chat-app-1b908.firebaseapp.com",
-  databaseURL: "https://my-chat-app-1b908-default-rtdb.firebaseio.com",
-  projectId: "my-chat-app-1b908",
-  storageBucket: "my-chat-app-1b908.appspot.com",
-  messagingSenderId: "212881613250",
-  appId: "1:212881613250:web:01e264cc1e26991f2af65b",
-  measurementId: "G-Y7XGW72HZB"
+  [my_firebase_config]
 };
 
 // Initialize Firebase
